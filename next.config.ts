3c689a0cd3a2823fc09728ecb14e7160d2e2c954
Davemafy <111160000+Davemafy@users.noreply.github.com>
2026-08-15 22:09:55 +0100

    Update nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+
   trailingSlash: true,
   poweredByHeader: false,
-  compress: true,
   reactStrictMode: true,
   productionBrowserSourceMaps: false,
-  async headers() {
-    return [
-      {
-        source: "/assets/:path*",
-        headers: [
-          { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
-        ],
-      },
-      {
-        source: "/favicon.svg",
-        headers: [
-          { key: "Cache-Control", value: "public, max-age=86400" },
-        ],
-      },
-    ];
+
+  images: {
+    unoptimized: true,
   },
 };
 
